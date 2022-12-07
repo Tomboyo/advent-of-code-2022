227f404e3f00e07c9287d67f1339dfc04909a75d
Tom Simmons <thsimmon@redhat.com>
2022-12-06 21:24:29 -0500

    again, forgot to save before commit

--- test/advent_2022/day2.clj
+++ test/advent_2022/day2.clj
@@ -3,6 +3,10 @@
             [advent-2022.day2 :as day2]))
 
 (deftest part1test
-  (testing "Day 2: Rock Paper Scissors"
+  (testing "Day 2 part 1"
     (is (= 15 (day2/part1 '("A Y" "B X" "C Z"))))))
+
+(deftest part2test
+  (testing "Day 2 part 2"
+    (is (= 12 (day2/part2 '("A Y", "B X", "C Z"))))))
 
