e3fa650e1027616d6a56d5cbea228e16f9ea05e2
Tom Simmons <thsimmon@redhat.com>
2022-12-06 21:17:47 -0500

    amend bad push

--- src/advent_2022/day2.clj
+++ src/advent_2022/day2.clj
@@ -4,7 +4,7 @@
 ;; Gives the score for the round based on what you played and the outcome of the round.
 ;; Rock, Paper, and Scissors throws are worth 1, 2, and 3 points respectively.
 ;; Losing, tying, and winning are woth 0, 3, and 6 points respectively.
-(defn score-round [s]
+(defn score-by-throw [s]
   (case s
     "A X" 4
     "A Y" 8
@@ -18,7 +18,7 @@
 
 ;; Similar to score-round-1, this determines how many points a round is worth usingthe same rules. However, the second argument of each round string indicates how the round needs to end, which implies the play that is thrown.
 ;; X, Y, and Z mean lose, draw, and win respectively.
-(defn score-round-2 [s]
+(defn score-by-outcome [s]
   (case s
     "A X" 3
     "A Y" 4
@@ -30,19 +30,21 @@
     "C Y" 6
     "C Z" 7))
 
-(defn scorer [score-round]
+;; Sums the score of all rounds using the given scoring strategy
+(defn score [strategy]
   (fn [lines]
-    (transduce (map score-round) + 0 lines)))
+    (transduce (map strategy) + 0 lines)))
 
-(defn part1 [lines] (apply (scorer score-round) lines))
+(def part1 (score score-by-throw))
+(def part2 (score score-by-outcome))
 
-(defn part2 [lines] (apply (scorer score-round-2) lines))
-
-(defn solve [resource scorer]
-  (let [url (jio/resource resource)]
+;; Load the problem from a file and apply the given solver to its contents
+(defn solve [solver]
+  (let [url (jio/resource "day2.txt")]
     (with-open [r (jio/reader url)]
-      (apply scorer (line-seq r)))))
+      (solver (line-seq r)))))
 
 (comment
-  (solve-part-1)
+  (solve part1)
+  (solve part2)
   :ref)
