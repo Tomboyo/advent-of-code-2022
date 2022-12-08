cdeb507d37089da6a4dd093caf649afd82b3929f
Tom Simmons <thsimmon@redhat.com>
2022-12-07 20:07:58 -0500

    refactor day 3 p 1

---
+++ src/advent_2022/bitset.clj
@@ -0,0 +1,29 @@
+(ns advent-2022.bitset)
+
+;; Convert a list of integers to a 64-bit set.
+(defn bitset64 [ilist]
+  (bit-shift-right (reduce bit-set 0 ilist) 1))
+
+(comment
+  ;; set the first four bits, giving the number 15
+  (bitset64 '(1 2 3 4))
+  ;; set the 52nd bit
+  (bitset64 '(52))
+  :ref)
+
+;; Return the intersection of two bitsets as a bitset.
+(defn intersection [a b] (bit-and a b))
+
+;; Return the position of the first element of the bitset, starting with the least significant bits.
+(defn first [x]
+  (loop [i x
+         p 0]
+    (if (= 0 i)
+      p
+      (recur (bit-shift-right i 1) (+ p 1)))))
+
+(comment
+  ;; The intersection of {1 6 7 8} and {1 2 3 4} is {1}.
+  ;; Note that this is a set.
+  (intersection (bitset64 '(1 6 7 8)) (bitset64 '(1 2 3 4)))
+  :ref)
--- src/advent_2022/day3.clj
+++ src/advent_2022/day3.clj
@@ -1,9 +1,10 @@
 (ns advent-2022.day3 
-  (:require [clojure.java.io :as jio]))
+  (:require [clojure.java.io :as jio]
+            [advent-2022.bitset :as bitset]))
 
-;; Given a string representing a rucksack, returns a list of character lists representing the compartments of the rucksack.
-(defn to-compartment-lists [line]
-  (split-at (/ (count line) 2) line))
+;; Divide a list in half into two equal sublists
+(defn bisect [list]
+  (split-at (/ (count list) 2) list))
 
 ;; Convert a character to its "priority"
 ;; character ascii ints:
@@ -20,57 +21,31 @@
       ;; a - z
       (+ 1 (- i 97)))))
 
-;; Given a list of character lists representing the compartments of a rucksack, return the equivalent list using prioritizes instead of characters.
-(defn prioritize [parts]
-  (map #(map priority %) parts))
-
-;; Get the position of the only set bit.
-;; Not that it matters, but our perf is already at least linear w.r.t. elements per compartment, so the naive strategy is fine
-(defn get-bit-pos [n]
-  (loop [i n
-         p 0]
-    (if (= 0 i)
-      p
-      (recur (bit-shift-right i 1) (+ p 1)))))
-(comment
-  ;; Get the position of the only set bit: 3
-  (get-bit-pos 4)
-  :ref)
-
-;; Convert the given list of integers to a bitmask, where each integer represents the position of the bit to set starting from 1, the least-significant bit.
-(defn mask [ipart]
-  (bit-shift-right (reduce bit-set 0 ipart) 1))
-(comment
-  ;; set the first four bits, giving the number 15
-  (mask '(1 2 3 4))
-  ;; set the 52nd bit
-  (mask '(52))
-  :ref)
-
-(defn get-common-priority [[a b]]
-  (get-bit-pos (bit-and (mask a) (mask b))))
-(comment
-  (get-common-priority [[1 2 3 4] [9 8 2 7]])
-  :ref)
-
-(def xform
-  (comp
-   ;; split the rucksack into compartments
-   (map to-compartment-lists)
-   ;; map elements of compartments to their prioritizes (1 - 52)
-   (map prioritize)
-   ;; find the common element priority between the compartments
-   (map get-common-priority)
-   ))
-
 (defn part1 [lines]
-  (transduce xform + 0 lines))
+  (let
+   [xform (comp
+           ;=> "abcAbC"
+           (map #(map priority %))
+           ;=> (1 2 3 27 2 29)
+           (map bisect)
+           ;=> [(1 2 3) (27 2 29)]
+           (map #(map bitset/bitset64 %))
+           ;=> [{1 2 3} {27 2 29}]
+           (map (fn [[a b]] (bitset/intersection a b)))
+           ;=> {2}
+           (map bitset/first)
+           ;=> 2
+           )]
+    (transduce xform + 0 lines)))
 
-;; Aplly the given solver line-by-line to the input file
+;; Apply a solver to the problem line-by-line
 (defn solve [solver]
   (let [url (jio/resource "day3.txt")]
     (with-open [r (jio/reader url)]
       (solver (line-seq r)))))
+
 (comment
+  (map priority "a")
   (solve part1)
-  )
+  :ref)
+  
