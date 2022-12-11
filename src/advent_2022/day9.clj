6827d12a0dbe7b0d1e19618a6e1fc9b92fb48fde
Tom Simmons <thsimmon@redhat.com>
2022-12-11 17:05:22 -0500

    day 9 part 2

--- src/advent_2022/day9.clj
+++ src/advent_2022/day9.clj
@@ -2,8 +2,7 @@
   (:require [advent-2022.util
              :refer
              [read-lines]]
-            [clojure.string :as str]
-            [clojure.tools.trace :refer [trace]]))
+            [clojure.string :as str]))
 
 ;; Return a function which translates a point in the indicated cardinal
 ;; direction.
@@ -55,19 +54,22 @@
     [(+ xt (ceil-abs (/ (- xh xt) 2)))
      (+ yt (ceil-abs (/ (- yh yt) 2)))]))
 
+;; A xform which reds the input of DIR COUNT statements into a flat list of
+;; xlate functions.
+(def to-xlate
+  (mapcat
+    (comp
+      (fn [[d n]] (for [_ (range n)] (xlate d)))
+      (fn [[d n]] [d (Integer/parseInt n)])
+      #(str/split % #"\s"))))
+
 ;; PART 1
 ;; Given H and T starting on the same arbitrary point, count the number of
 ;; spaces that T occupies as it follows H.
 (println
   "PART 1: "
   (transduce
-    ;; Reads the input of DIR COUNT statements into a flat list of xlate
-    ;; functions.
-    (mapcat
-      (comp
-        (fn [[d n]] (for [_ (range n)] (xlate d)))
-        (fn [[d n]] [d (Integer/parseInt n)])
-        #(str/split % #"\s")))
+    to-xlate
     ;; Transforms the positions of H and T, accumulating the points that T
     ;; occupies over time.
     (fn [[acc h t] & [move]]
@@ -84,3 +86,27 @@
 
 ;; PART 2
 ;; Like part 1, but now there 9 instances of T that follow along in a chain.
+(println
+  "PART 2: "
+  (transduce
+    to-xlate
+    ;; Transforms the positions of H and Ti, accumulating the points that T9
+    ;; occupies over time.
+    (fn [[acc xs] & [move]]
+      (if (nil? move)
+        ;; On EOF, return the number of distinct positions held by T9
+        (count acc)
+        ;; Otherwise move H, then the Ti, an return the updated acc and
+        ;; positions of H and Ti.
+        (loop [a (move (first xs))
+               b (second xs)
+               xs (drop 2 xs)
+               xs' [a]]
+          (let [b' (follow a b)]
+            (if (empty? xs)
+              ;; b' is T9. We're done.
+              [(conj acc b') (conj xs' b')]
+              (recur b' (first xs) (rest xs) (conj xs' b')))))))
+    ;; Initially, H and T1 - T9 start on the same arbitrary point.
+    [#{[0 0]} (repeat 10 [0 0])]
+    (read-lines "day9.txt")))
