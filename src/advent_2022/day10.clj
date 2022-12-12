4ad51f13b9696b50e8594788ead6bdc8940f5531
Tom Simmons <thsimmon@redhat.com>
2022-12-11 22:11:55 -0500

    day 10 part 2

--- src/advent_2022/day10.clj
+++ src/advent_2022/day10.clj
@@ -1,6 +1,5 @@
 (ns advent-2022.day10
-  (:require [advent-2022.util :refer [read-lines-eager]]
-            [clojure.tools.trace :refer [trace]]))
+  (:require [advent-2022.util :refer [read-lines-eager]]))
 
 (defn run-machine
   "Produces a lazy sequence of X register states indexed by clock cycle, [c x]"
@@ -12,7 +11,6 @@
   ;; f: the function to aply to x
   ;; c: the number of cycles remaining until f is applied to x
   ([x program cycle [f c debug]]
-   (trace ["CYCLE" cycle  "X" x "C" c "CMD" debug])
    (cond
      ;; The program finished.
      (nil? f) nil
@@ -23,13 +21,13 @@
                    cmd (first program)
                    program (rest program)
                    cycle' (+ cycle 1)]
-               (lazy-seq (cons (trace "EMIT" [cycle x])
+               (lazy-seq (cons [cycle x]
                                (run-machine x' program cycle' cmd))))
      ;; The cmd is still running, so we need to decrement its counter and keep
      ;; going.
      :default (let [cmd [f (- c 1)]
                     cycle' (+ cycle 1)]
-                (lazy-seq (cons (trace "EMIT" [cycle x])
+                (lazy-seq (cons [cycle x]
                                 (run-machine x program cycle' cmd)))))))
 
 ;; Parses instruction lines into cmds whose first element is a function to apply
@@ -50,7 +48,7 @@
                       ; Filter to cycles 20, 60, 100, etc, mapped to their signal strength
                       (comp
                         (filter (fn [[c _]] (= 20 (mod c 40))))
-                        (map (fn [[c x]] (trace (str "STRENGTH-" c) (* c x)))))
+                        (map (fn [[c x]] (* c x))))
                       ; Sum of signal strengths
                       +
                       ; Generate the first 180 cycles of state for this program.
@@ -59,3 +57,27 @@
                            (map parse-command)
                            (run-machine 1)
                            (take 220))))
+
+;; Part 2
+;; Paint the screen!
+(println "--- PART 2 ---\n"
+         (transduce
+           (comp
+             ; Determine what the CRT should paint. Cycle-mod-40 is the
+             ; horizontal position to paint, and we should paint "#" if the
+             ; sprite intersects the CRT. The sprite is 3 pixels wide, and X
+             ; indicates its center. "." is painted otherwise.
+             ; Remember that c is one-based, and should be ashamed of itself.
+             (map (fn [[c x]] (if (<= (- x 1) (mod (- c 1) 40) (+ x 1)) "#" ".")))
+             ; Partition the output into 40-pixel-wide rows
+             (partition-all 40)
+             ; Make those lines into strings, adding newlines
+             (map #(apply str (cons "\n" %))))
+           ; Concat each line of CRT drawing
+           str
+           ; Generate the first 180 cycles of state for this program.
+           ; See also day10.test.txt
+           (->> "day10.txt"
+                read-lines-eager
+                (map parse-command)
+                (run-machine 1))))
