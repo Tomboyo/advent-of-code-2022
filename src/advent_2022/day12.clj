d968a89e60f4e34ae990b221c1e513cb37c9873a
Tom Simmons <thsimmon@redhat.com>
2022-12-14 00:00:07 -0500

    solve 12 part 2

--- src/advent_2022/day12.clj
+++ src/advent_2022/day12.clj
@@ -1,6 +1,7 @@
 (ns advent-2022.day12
   (:require [clojure.java.io :as jio]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.tools.trace :refer [trace]]))
 
 ; PART 1
 (def input (->> (jio/resource "day12.txt")
@@ -21,7 +22,9 @@
 (def S (find-coord \S input))
 (def E (find-coord \E input))
 
-(defn neighbors [graph [y x :as p]]
+(defn neighbors-up-1 [graph [y x :as p]]
+  "Returns neighbors in the cardinal directions which are no more than one
+  z-level higher than the origin"
   (let [max-height (+ 1 (get-in graph p))
         up [(- y 1) x]
         down [(+ y 1) x]
@@ -32,27 +35,58 @@
          (filter (fn [q]
                    (<= (get-in graph q) max-height))))))
 
-(defn BFS
-  [graph start end]
+; For part 2, we can start the search from E and basically invert the traversal
+; rules to imitate walking in the opposite direction. SO, instead of being able
+; to go at most one level up and any levels down, we can go any levels up and at
+; most one down.
+(defn neighbors-down-1 [graph [y x :as p]]
+  "Returns neighbors in the cardinal directions which are no more than one
+  z-level lower than the origin."
+  (let [min-height (- (get-in graph p) 1)
+        up [(- y 1) x]
+        down [(+ y 1) x]
+        left [y (- x 1)]
+        right [y (+ x 1)]]
+    (->> (list up down left right)
+         (filter (fn [q] (not (nil? (get-in graph q)))))
+         (filter (fn [q]
+                   (>= (get-in graph q) min-height))))))
+
+(defn bfs
+  "Performs breadth-first search on the graph from the starting point until the
+  end function returns true. Neighbors defines which points in the graph are
+  reachable from a point."
+  [graph neighbors start end]
   (loop [parents {start :start}
          horizon [start]
-         visited #{}]
+         visited #{start}]
     (let [x (first horizon)]
-      (if (= end x)
-        parents
+      (assert (not (nil? x)) "neighbors did not return points")
+      (if (end graph x)
+        [x parents]
         (let [next (->> (neighbors graph x)
                         (filter (comp not visited)))]
           (recur (reduce (fn [p n] (assoc p n x)) parents next)
                  (into [] (concat (subvec horizon 1) next))
                  (into #{} (concat visited next))))))))
 
+(defn path-length [[path-end paths]]
+  (loop [counter 1
+         x (paths path-end)]
+    (let [parent (paths x)]
+      (if (= :start parent)
+        counter
+        (recur (+ counter 1) parent)))))
+
 ; Part 1
 (println
-  "Part 1: "
-  (let [path (BFS G S E)]
-    (loop [counter 1
-           x (path E)]
-      (let [parent (path x)]
-        (if (= :start parent)
-          counter
-          (recur (+ counter 1) parent))))))
+  "Part 1:"
+  (path-length (bfs G neighbors-up-1 S (fn [_graph x] (= E x)))))
+
+; Part 2
+; Both part 1 and part 2 are the same problem if we parameterize the starting
+; point, how to traverse, and how to identify the stopping point.
+(println
+  "Part 2:"
+  (path-length
+    (bfs G neighbors-down-1 E (fn [graph x] (= (get-in graph x) (int \a))))))
