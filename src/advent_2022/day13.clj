21a74f71c9d1acfedfae58118e5bb108ff0a1012
Tom Simmons <thsimmon@redhat.com>
2022-12-14 21:40:44 -0500

    day 13 part 2!

--- src/advent_2022/day13.clj
+++ src/advent_2022/day13.clj
@@ -1,47 +1,61 @@
 (ns advent-2022.day13
   (:require [clojure.java.io :as jio]
-            [clojure.string :as str]
-            [clojure.tools.trace :refer [trace]]))
+            [clojure.string :as str]))
 
 (defn parse-input [seq]
   (->> seq
        (map #(str/replace % "," " "))
        (filter (comp not str/blank?))
-       (map read-string)
-       (partition 2)))
+       (map read-string)))
 
 (defn inorder? [left right]
   (cond
     (and (number? left) (number? right))
-      (cond (< left right) :<
-            (= left right) :=
-            :default :>)
+    (cond (< left right) :<
+          (= left right) :=
+          :default :>)
     (and (sequential? left) (sequential? right))
-      (loop [xs left
-             ys right]
-        (let [x (first xs) y (first ys)]
-          (cond
-            (and x y) (case (inorder? x y)
-                        :< :<
-                        := (recur (rest xs) (rest ys))
-                        :> :>)
-            x :>
-            y :<
-            ; Both (nested) lists exhausted
-            (and (nil? x) (nil? y)) :=
-            :default (assert false "Default"))))
+    (loop [xs left
+           ys right]
+      (let [x (first xs) y (first ys)]
+        (cond
+          (and x y) (case (inorder? x y)
+                      :< :<
+                      := (recur (rest xs) (rest ys))
+                      :> :>)
+          x :>
+          y :<
+          ; Both (nested) lists exhausted
+          (and (nil? x) (nil? y)) :=
+          :default (assert false "Default"))))
     (sequential? left)
-      (inorder? left [right])
+    (inorder? left [right])
     (sequential? right)
-      (inorder? [left] right)))
+    (inorder? [left] right)))
 
-(inorder? [[1] [2 3 4]]
-          [[1] 4])
+(def input (slurp (jio/resource "day13.txt")))
 
-(with-open [r (jio/reader (jio/resource "day13.txt"))]
-  (->> (line-seq r)
-       (parse-input)
-       (map (fn [[left right]] (= :< (inorder? left right))))
-       (keep-indexed (fn [index, bool] (if bool (+ 1 index) nil)))
-       (reduce + 0)
-       (println "Part 1:")))
+; Part 1
+(->> (str/split-lines input)
+     (parse-input)
+     (partition 2)
+     (map (fn [[left right]] (= :< (inorder? left right))))
+     (keep-indexed (fn [index, bool] (if bool (+ 1 index) nil)))
+     (reduce + 0)
+     (println "Part 1:"))
+
+; Part 2
+(->> (str/split-lines input)
+     (parse-input)
+     (concat [[[2]] [[6]]])
+     (sort (fn [left right] (case (inorder? left right)
+                              :< -1
+                              := 0
+                              :> 1)))
+     (keep-indexed (fn [index, packet]
+                     (if
+                       (or (= [[2]] packet) (= [[6]] packet))
+                       (+ index 1)
+                       nil)))
+     (reduce *)
+     (println "Part 2:"))
